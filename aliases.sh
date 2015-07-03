423dcc37ff157a671789124f97c5ebd801164d98
Ben Barnett <ben.barnett@moneyadviceservice.org.uk>
2015-07-03 16:24:56 +0100

    Adding more aliases

--- aliases.sh
+++ aliases.sh
@@ -1,2 +1,8 @@
+export EDITOR="subl -w"
+
 alias mysql_start="/usr/local/bin/mysql.server start"
 alias flush_dns="sudo discoveryutil udnsflushcaches"
+alias webserver="python -m SimpleHTTPServer 8080"
+
+alias be="bundle exec"
+alias ll="ls -hlF"
